1df107606eec10aff15017827b77c77fa5e326c9
J Martin <72631437+tikcuf2@users.noreply.github.com>
2021-06-08 16:03:07 -0700

    math

---
+++ week1/Math/app.js
@@ -0,0 +1,7 @@
+const mathOutput = require('./math');
+
+console.log(mathOutput);
+console.log(mathOutput.sum(14,9));
+console.log(mathOutput.subtract(10,3));
+console.log(mathOutput.divide(100,10));
+console.log(mathOutput.multiply(10,3));
